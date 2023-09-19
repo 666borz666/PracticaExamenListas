767f27bd16b47b233ba2d3533959894bb17a6b76
666borz666 <iloveass3333@gmail.com>
2023-09-19 13:25:52 -0600

    Reto 13 y 14

--- funciones.py
+++ funciones.py
@@ -37,6 +37,41 @@ def productoCartesiano(A, B):
 
 #Reto 10
 
+#Reto 12
+
+#Reto 13
+def encontrarCiudadanos(n,cedulas):
+    """
+    """
+    provincia=""
+    ced=[]
+    if n>=1 or n<=7:
+        for a in cedulas:
+            cedula=str(a)
+            provincia=int(cedula[0])
+            if provincia==n:
+                ced.append(a)
+        return ced
+    else:
+        return print("La provincia debe ser un valor entre 1 y 7.")
+        
+        
+#Reto 14
+def extraerNumeros(txt):
+    """
+    """
+    num=[]
+    numero = ""
+    for a in txt:
+        if a.isdigit() or a == ".":
+            numero += a 
+        elif numero:
+            num.append(float(numero))
+            numero = ""
+    if numero:
+        num.append(float(numero))
+    return num
+
 #programa principal
 
 """
@@ -57,4 +92,16 @@ B=["a","b"]
 print(productoCartesiano(A, B))
 """
 
-#Reto 10
+#Reto 13
+print(encontrarCiudadanos(2,[123456789, 201340789, 222222222 ,433333333, 511111111, 207890456]))
+print(encontrarCiudadanos (1,[123456789,213456789,222222222,233333333,511111111,207890456]))
+print(encontrarCiudadanos (3,[123456789,213456789,222222222,423333333,511111111,207890456]))
+print((9,[123456789,213456789,222222222,233333333,511111111,207890456]))
+
+"""
+#Reto 14
+print(extraerNumeros("xy225p30ab0g8.9iou1000"))
+print(extraerNumeros("ab95.8124c76n"))
+print(extraerNumeros("x0.25za10.5"))
+print(extraerNumeros("xyz"))
+"""
